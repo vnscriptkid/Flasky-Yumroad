e3e83b849d9f3b3a1b5c5c22942b71b510cfc439
Thanh Nguyen <vnscriptkid@gmail.com>
2021-03-10 23:20:48 +0700

    Add Product model

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from app.extensions import db
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(120), nullable=False)
+
