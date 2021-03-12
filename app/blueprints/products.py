06529d3d76695e89287e16d1498736e157e63eb6
Thanh Nguyen <vnscriptkid@gmail.com>
2021-03-12 22:19:41 +0700

    Add index and details routes for products

--- app/blueprints/products.py
+++ app/blueprints/products.py
@@ -1,9 +1,26 @@
 from flask import Blueprint, render_template
+from werkzeug.exceptions import abort
+
+from app.models import Product
 
 products = Blueprint('products', __name__)
 
 
 @products.route('/')
 def index():
-    return "All Products: Coming soon"
+    all_products = Product.query.all()
+    return render_template('products/index.html', products=all_products)
+
+
+@products.route('/<int:product_id>')
+def details(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+        abort(404)
+    return render_template('products/details.html', product=product)
+
+
+@products.errorhandler(404)
+def not_found(exception):
+    return render_template('products/404.html'), 404
 
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,7 +1,16 @@
 import pytest
+from flask import url_for
 
 from app import db
 from app.models import Product
+
+
+@pytest.fixture
+def sample_book(name="Sherlock Homes", description="a house hunting real estate agent"):
+    book = Product(name=name, description=description)
+    db.session.add(book)
+    db.session.commit()
+    return book
 
 
 def test_product_creation(client, init_database):
@@ -16,3 +25,26 @@ def test_product_creation(client, init_database):
 def test_name_validation(client, init_database):
     with pytest.raises(ValueError):
         Product(name=" ", description="invalid book")
+
+
+def test_index_page(client, init_database, sample_book):
+    response = client.get(url_for('products.index'))
+    assert response.status_code == 200
+    assert b'Yumroad' in response.data
+    assert sample_book.name in str(response.data)
+    expected_link = url_for('products.details', product_id=sample_book.id)
+    assert expected_link in str(response.data)
+
+
+def test_details_page(client, init_database, sample_book):
+    response = client.get(url_for('products.details', product_id=sample_book.id))
+    assert response.status_code == 200
+    assert b'Yumroad' in response.data
+    assert b'Purchase coming soon' in response.data
+    assert sample_book.name in str(response.data)
+
+
+def test_non_existent_book(client, init_database, sample_book):
+    response = client.get(url_for('products.details', product_id=sample_book.id+1))
+    assert response.status_code == 404
+
