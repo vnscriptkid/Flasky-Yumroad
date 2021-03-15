9498f25157d3705ee5360b388351d296ce8437e0
Thanh Nguyen <vnscriptkid@gmail.com>
2021-03-15 11:41:44 +0700

    Add pagination for store show

--- app/blueprints/stores.py
+++ app/blueprints/stores.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-from app.models import Store
+from app.models import Store, Product
 
 store_bp = Blueprint('store', __name__)
 
@@ -11,9 +11,11 @@ def index():
     return render_template('stores/index.html', stores=stores)
 
 
-@store_bp.route('/store/<store_id>')
-def show(store_id):
+@store_bp.route('/stores/<store_id>')
+@store_bp.route('/stores/<store_id>/<int:page>')
+def show(store_id, page=1):
     store = Store.query.get_or_404(store_id)
-    products = store.products
-    return render_template('stores/show.html', store=store, products=products)
+    per_page = 9
+    products_pagination = Product.query.filter_by(store=store).paginate(page, per_page)
+    return render_template('stores/show.html', store=store, products_pagination=products_pagination)
 
